e86f041bedf917cc535cef735330d4908413d354
Rockosov <rockosov@gmail.com>
2012-05-11 12:55:15 +0400

    Perceptron model blanks

---
+++ src/Perceptron.h
@@ -0,0 +1,16 @@
+//
+//  Perceptron.h
+//  iNeuroNet
+//
+//  Created by rockosov on 30.04.12.
+//  Copyright 2012 TIT_SFEDU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Perceptron : NSObject {
+	
+}
+
+@end
---
+++ src/Perceptron.m
@@ -0,0 +1,14 @@
+//
+//  Perceptron.m
+//  iNeuroNet
+//
+//  Created by rockosov on 30.04.12.
+//  Copyright 2012 TIT_SFEDU. All rights reserved.
+//
+
+#import "Perceptron.h"
+
+
+@implementation Perceptron
+
+@end
